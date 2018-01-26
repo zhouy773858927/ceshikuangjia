0647cb7f08e66781af011e3a1543fce38ded4ba7
zhouy <773858927@qq.com>
2018-01-26 14:18:03 +0800

    将测试报告在test_baidu用例里面单元测试

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -3,9 +3,10 @@ import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from utils.config import Config , DRIVER_PATH,DATA_PATH
+from utils.config import Config , DRIVER_PATH,DATA_PATH,REPORT_PATH
 from utils.log import logger
 from utils.file_reader import ExcelReader
+from utils.HTMLTestRunner import HTMLTestRunner
 
 class TestBaidu(unittest.TestCase):
     URL = Config().get('URL')
@@ -13,6 +14,7 @@ class TestBaidu(unittest.TestCase):
     logger.info("url is %s" % URL)
     locator_kw = (By.ID , 'kw')
     locator_su = (By.ID , 'su')
+    locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
     def sub_setUp(self):
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe')
@@ -32,7 +34,13 @@ class TestBaidu(unittest.TestCase):
                 self.driver.find_element(*self.locator_su).click()
                 time.sleep(2)
                 logger.info("now is search %s"% d["search"])
+                links = self.driver.find_elements(*self.locator_result)
+                for link in links:
+                    logger.info(link.text)
                 self.sub_tearDown()
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    report = REPORT_PATH + '\\report.html'
+    with open(report ,'wb') as  f:
+        runner = HTMLTestRunner(f, verbosity=2 ,title='测试框架',description='修改过的测试报告')
+        runner.run(TestBaidu("test_search_0"))
